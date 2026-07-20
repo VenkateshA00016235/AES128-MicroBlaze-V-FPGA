985f7b3389e8af760944446ff466b7fe010737b6
VenkateshA00016235 <venkatesh.maddukuri2@mail.dcu.ie>
2026-07-21 00:18:05 +0100

    Ignore unused ARM projects and boot outputs

--- _ide/workspace_journal_17480.py
+++
@@ -1,8 +0,0 @@
-# 2026-06-18T19:12:16.158379700
-import vitis
-
-client = vitis.create_client()
-client.set_workspace(path="D:/AES128-MicroBlaze-V-FPGA")
-
-vitis.dispose()
-
--- _ide/workspace_journal_3796.py
+++
@@ -1,8 +0,0 @@
-# 2026-06-17T21:25:19.281714100
-import vitis
-
-client = vitis.create_client()
-client.set_workspace(path="D:/AES128-MicroBlaze-V-FPGA")
-
-vitis.dispose()
-
